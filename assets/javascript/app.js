19eed492fc08169db1a644ae79ac70e33c7ce639
lgfahys <lindsey@codeasite.com>
2019-03-19 15:59:25 -0400

    added basic html wireframe, added user name input form/connected name to firebase, dynamically created user choice buttons & created a function to grab the name of each button that was clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,100 @@
 $(document).ready(function () {
 
-//initializing firebase
-var config = {
-    apiKey: "AIzaSyC5mKilH9BwCWjw9wEXFKNMyOZ7OFFvzdQ",
-    authDomain: "rps-multiplayer-d0c83.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-d0c83.firebaseio.com",
-    projectId: "rps-multiplayer-d0c83",
-    storageBucket: "rps-multiplayer-d0c83.appspot.com",
-    messagingSenderId: "340963260873"
-};
+    //initializing firebase
+    var config = {
+        apiKey: "AIzaSyC5mKilH9BwCWjw9wEXFKNMyOZ7OFFvzdQ",
+        authDomain: "rps-multiplayer-d0c83.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-d0c83.firebaseio.com",
+        projectId: "rps-multiplayer-d0c83",
+        storageBucket: "rps-multiplayer-d0c83.appspot.com",
+        messagingSenderId: "340963260873"
+    };
 
-firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-//setting global variables
-var database = firebase.database();
+    //referencing the database
+    var database = firebase.database();
 
-var userOne = {
-    name: "",
-    wins: 0,
-    losses: 0,
-    choice: "",
-}
+    //setting global variables
 
-var userTwo = {
-    name: "",
-    wins: 0,
-    losses: 0,
-    choice: "",
-}
+    // var userOne = {
+    //     name: "",
+    //     wins: 0,
+    //     losses: 0,
+    //     choice: "",
+    // }
 
-//when user clicks a key
-$(document).keyup(function () {
+    // var userTwo = {
+    //     name: "",
+    //     wins: 0,
+    //     losses: 0,
+    //     choice: "",
+    // }
 
-    if (((userOneGuess === "r") || (userOneGuess === "p") || (userOneGuess === "s"))
-        && ((userTwoGuess === "r") || (userTwoGuess === "p") || (userTwoGuess === "s"))) {
+    var choices = ["Rock", "Paper", "Scissors"];
 
-        if ((userOneGuess === "r" && userTwoGuess === "s") ||
-            (userOneGuess === "s" && userTwoGuess === "p") ||
-            (userOneGuess === "p" && userTwoGuess === "r")) {
-            userOne.wins++ && userTwo.losses--;
-        } else if (userOneGuess === userTwoGuess) {
-            userOne.ties++ && userTwo.ties++;
-        } else {
-            userOne.losses++ && userTwo.wins++;
+    $("#add-name").on("click", function (event) {
+        event.preventDefault();
+
+        //grabbing what the user input as their name
+        name = $("#name-input").val().trim();
+        $("#user-name").text("Hello, " + name);
+        //set name value in database
+        database.ref().set({
+            name: name,
+        });
+
+    });
+
+    database.ref().on("value", function (snapshot) {
+        console.log(snapshot.val().name);
+
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+
+    function renderButtons() {
+
+        //looping through the array of choices
+        for (var i = 0; i < choices.length; i++) {
+            //dynamically creating buttons with jquery for all choices
+            var btn = $("<button>");
+            btn.addClass("choices");
+            btn.attr("data-name", choices[i]);
+            btn.text(choices[i]);
+            $("#user-choices").append(btn);
         }
     }
-    alert("yo");
+
+    //capturing user choice by data attribute
+    function getUserChoice() {
+        var userChoice = $(this).attr("data-name");
+        console.log(userChoice);
+        $("#user-one-choice").text("You chose: " + userChoice);
+    }
+
+    $(document).on("click", ".choices", getUserChoice);
+
+
+    renderButtons();
 });
 
-});
+    // //when user clicks a key
+    // $(document).keyup(function () {
+
+    //     // if (((userOneGuess === "r") || (userOneGuess === "p") || (userOneGuess === "s"))
+    //     //     && ((userTwoGuess === "r") || (userTwoGuess === "p") || (userTwoGuess === "s"))) {
+
+    //     //     if ((userOneGuess === "r" && userTwoGuess === "s") ||
+    //     //         (userOneGuess === "s" && userTwoGuess === "p") ||
+    //     //         (userOneGuess === "p" && userTwoGuess === "r")) {
+    //     //         userOne.wins++ && userTwo.losses--;
+    //     //     } else if (userOneGuess === userTwoGuess) {
+    //     //         userOne.ties++ && userTwo.ties++;
+    //     //     } else {
+    //     //         userOne.losses++ && userTwo.wins++;
+    //     //     }
+    //     // }
+
+    // });
