5c382868df71af122e3bf75ece3cc72bff3542fb
lgfahys <lindsey@codeasite.com>
2019-03-22 17:01:03 -0400

    starting over.. got firebase to track player one and two

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,98 +17,102 @@ $(document).ready(function () {
 
     //setting global variables
 
-    var numPlayers = 0;
     var playerOne = "";
     var playerTwo = "";
-    var name = "";
+    var playerOneName = "";
+    var playerTwoName = "";
+    var thisPlayer = "";
+    var numTurns = 0;
 
     var choices = ["Rock", "Paper", "Scissors"];
 
-    //hiding until user enters their information
-    function startGame() {
-        $("#sign-out").hide();
-        $("#user-one-choice").hide();
-        $("#user-two-choice").hide();
-        $("#user-choices").hide();
-        $("#opponent-choices").hide();
-        clearFirebase();
-    }
+    //hiding stuff on initial page load (update this to hidden only if thisPlayer does not exist)
+    $("#sign-out").hide();
+    $("#user-one-choice").hide();
+    $("#user-two-choice").hide();
 
+    // //when player submits their name
     $("#add-info").on("click", function (event) {
         event.preventDefault();
-        numPlayers++;
-        //making sure a blank info is not input
-        if ($("#name-input").val() !== "") {
-            //grabbing what the user input as their name
-            name = $("#name-input").val().trim();
-            $("#user-name").text("Hello, " + name);
-            $("#user-input").hide();
-            $("#instructions").text("Select rock, paper or scissors");
-            $("#sign-out").show();
-            $("#user-one-choice").show();
-            $("#user-two-choice").show();
-            $("#user-choices").show();
-        } else {
-            alert("Please input your name to begin");
+        //making sure a blank name is not input & checking if there is not player one or two
+        if (($("#name-input").val().trim() !== "")) {
+            if ((playerOne === null) && (playerTwo === null)) {
+                thisPlayer = $("#name-input").val().trim();
+                $("#user-name").text("Hello, " + thisPlayer);
+
+                playerOne = {
+                    name: thisPlayer,
+                    choice: "",
+                    wins: 0,
+                    losses: 0,
+                    ties: 0,
+                };
+
+                //setting player info in the database
+                database.ref().child("/players/playerOne").set(playerOne);
+                //if player one exists and player two does not
+            } else if ((playerOne !== null) && (playerTwo === null)) {
+                thisPlayer = $("#name-input").val().trim();
+                $("#user-name").text("Hello, " + thisPlayer);
+
+                playerTwo = {
+                    name: thisPlayer,
+                    choice: "",
+                    wins: 0,
+                    losses: 0,
+                    ties: 0,
+                };
+
+                //setting player info in the database
+                database.ref().child("/players/playerTwo").set(playerTwo);
+            }
         }
+
+        $("#name-input").val("");
     });
 
-    $("#sign-out").on("click", function (event) {
-        startGame();
-    });
+    database.ref("/players/").on("value", function (snapshot) {
 
-    function renderButtons() {
+        // $("#user-name").text(snapshot.val().thisPlayer);
 
-        //looping through the array of choices
-        for (var i = 0; i < choices.length; i++) {
-            //dynamically creating buttons with jquery for all choices
-            btn = $("<button>");
-            btn.addClass("choices");
-            btn.attr("data-name", choices[i]);
-            btn.text(choices[i]);
-            $("#user-choices").append(btn);
-        }
-    }
-
-    //capturing user choice by data attribute
-    function getUserChoice() {
-        var choice = $(this).attr("data-name");
-        $("#user-one-choice").text("You chose: " + choice);
-        console.log(numPlayers);
-        if (numPlayers === 1) {
-            playerValue = 1;
+        if (snapshot.child("playerOne").exists()) {
+            playerOne = snapshot.val().playerOne;
+            playerOneName = playerOne.name;
+            console.log("Player One: " + playerOneName);
+            $("#user-one-choice").append(playerOneName);
+            $("#user-one-choice").append("Wins: " + playerOne.wins + "Losses " + playerOne.losses + "Tie " + playerOne.ties);
         } else {
-            playerValue = 2;
+            playerOne = null;
+            playerOneName = "";
+            $("#instructions").text("Waiting for player 1");
         }
-        database.ref().push({
-            name: name,
-            choice: choice,
-            playerValue: playerValue,
-        });
-    }
 
-    function pairUsers() {
+        if (snapshot.child("playerTwo").exists()) {
+            playerTwo = snapshot.val().playerTwo;
+            playerTwoName = playerTwo.name;
+            console.log("Player Two: " + playerTwoName);
+            $("#user-two-choice").append(playerTwoName);
+            $("#user-two-choice").append("Wins: " + playerTwo.wins + "Losses " + playerTwo.losses + "Tie " + playerTwo.ties);
+        } else {
+            playerTwo = null;
+            playerTwoName = "";
+            $("#instructions").text("Waiting for player 2");
+        }
 
-    }
-
-    $(document).on("click", ".choices", getUserChoice);
-
-    database.ref().on("child_added", function (snapshot) {
-        console.log(snapshot.val().name);
-        console.log(snapshot.val().choice);
-        console.log(snapshot.val().playerValue);
-        $("#user-name").text("Hello, " + snapshot.val().name);
-
+        if (playerOne && playerTwo) {
+            $("#user-one-choice").css("border-color", "#ffffff", "border-weight", "1px", "border-style", "solid");
+            $("#instructions").text("Player 1's turn")
+        }
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
 
-    renderButtons();
 
-    // creating a function to clear the database to be used when users sign out or time out
-    function clearFirebase () {
-        database.ref().remove();
-    }
+    //clearing the database when a user signs out
+    $("#sign-out").on("click", function (event) {
+        database.ref().remove(thisPlayer);
+    });
+
 
 });
 
@@ -130,3 +134,48 @@ $(document).ready(function () {
     //     // }
 
     // });
+
+
+        // $("#sign-out").on("click", function (event) {
+        //     startGame();
+        // });
+
+        // function renderButtons() {
+
+        //     //looping through the array of choices
+        //     for (var i = 0; i < choices.length; i++) {
+        //         //dynamically creating buttons with jquery for all choices
+        //         btn = $("<button>");
+        //         btn.addClass("choices");
+        //         btn.attr("data-name", choices[i]);
+        //         btn.text(choices[i]);
+        //         $("#user-choices").append(btn);
+        //     }
+        // }
+
+        // //capturing user choice by data attribute
+        // function getUserChoice() {
+        //     var choice = $(this).attr("data-name");
+        //     $("#user-one-choice").append("You chose: " + choice);
+        // }
+
+        // //getting user choice on choice button click
+        // $(document).on("click", ".choices", getUserChoice);
+
+        // database.ref().on("value", function (snapshot) {
+        //     // console.log(snapshot.val().name);
+        //     // console.log(snapshot.val().choice);
+        //     $("#user-name").text("Hello, " + snapshot.val().thisPlayer);
+
+        // }, function (errorObject) {
+        //     console.log("Errors handled: " + errorObject.code);
+        // });
+
+        // renderButtons();
+
+        // // creating a function to clear the database to be used when users sign out or time out
+        // function clearFirebase () {
+        //     database.ref().remove();
+        // }
+
+
