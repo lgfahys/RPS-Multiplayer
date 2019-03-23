ddfa7fa7cedb0aa42c56c220931da0d945ebd465
lgfahys <lindsey@codeasite.com>
2019-03-22 21:58:55 -0400

    got player choices to update in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,16 +22,18 @@ $(document).ready(function () {
     var playerOneName = "";
     var playerTwoName = "";
     var thisPlayer = "";
-    var numTurns = 0;
+    var numTurns = 1;
 
     var choices = ["Rock", "Paper", "Scissors"];
 
     //display setup on initial page load
     $("#sign-out").hide();
     $("#instructions").text("Enter your name to begin playing");
-    $("#user-one-choice").hide();
-    $("#user-two-choice").hide();
+    $("#user-one-div").hide();
+    $("#user-two-div").hide();
     $("#user-input").hide();
+    $("#user-choices").hide()
+    $("#opponent-choices").hide()
 
     // //when player submits their name
     $("#add-info").on("click", function (event) {
@@ -81,25 +83,27 @@ $(document).ready(function () {
             playerOne = sv.playerOne;
             playerOneName = playerOne.name;
             console.log("Player One: " + playerOneName);
+            $("#instructions").text("Waiting for Player Two");
             $("#user-input").hide();
-            $("#user-one-choice").show();
-            $("#user-two-choice").show();
+            $("#user-one-div").show();
+            $("#user-two-div").show();
             $("#user-one-name").text("Player One: " + playerOneName);
             //ensuring user one choice div does not repeat text when player 2 is added
-            $("#user-one-choice").empty();
-            $("#user-one-choice").append(playerOneName);
-            $("#user-one-choice").append("<br>");
-            $("#user-one-choice").append("Wins: " + playerOne.wins);
-            $("#user-one-choice").append("<br>");
-            $("#user-one-choice").append("Losses: " + playerOne.losses);
-            $("#user-one-choice").append("<br>");
-            $("#user-one-choice").append("Ties: " + playerOne.ties);
-            $("#user-one-choice").append("<br>");
+            $("#user-one-div").empty();
+            //appending all player one info
+            $("#user-one-div").append(playerOneName);
+            $("#user-one-div").append("<br>");
+            $("#user-one-div").append("Wins: " + playerOne.wins);
+            $("#user-one-div").append("<br>");
+            $("#user-one-div").append("Losses: " + playerOne.losses);
+            $("#user-one-div").append("<br>");
+            $("#user-one-div").append("Ties: " + playerOne.ties);
+            $("#user-one-div").append("<br>");
         } else {
             $("#user-input").show();
             playerOne = null;
             playerOneName = "";
-            $("#instructions").text("Waiting for player 1");
+            $("#instructions").text("Waiting for Player One");
         }
 
         if (snapshot.child("playerTwo").exists()) {
@@ -107,17 +111,20 @@ $(document).ready(function () {
             playerTwo = sv.playerTwo;
             playerTwoName = playerTwo.name;
             console.log("Player Two: " + playerTwoName);
-            $("#instructions").text("Enter your name to begin playing");
+            $("#instructions").text("Player One's turn");
             $("#user-input").hide();
             $("#user-two-name").text("Player Two: " + playerTwoName);
-            $("#user-two-choice").append(playerTwoName);
-            $("#user-two-choice").append("<br>");
-            $("#user-two-choice").append("Wins: " + playerTwo.wins);
-            $("#user-two-choice").append("<br>");
-            $("#user-two-choice").append("Losses: " + playerTwo.losses);
-            $("#user-two-choice").append("<br>");
-            $("#user-two-choice").append("Ties: " + playerTwo.ties);
-            $("#user-two-choice").append("<br>");
+            //ensuring user one choice div does not repeat text when player 2 is added
+            $("#user-two-div").empty();
+            //appending all player two info
+            $("#user-two-div").append(playerTwoName);
+            $("#user-two-div").append("<br>");
+            $("#user-two-div").append("Wins: " + playerTwo.wins);
+            $("#user-two-div").append("<br>");
+            $("#user-two-div").append("Losses: " + playerTwo.losses);
+            $("#user-two-div").append("<br>");
+            $("#user-two-div").append("Ties: " + playerTwo.ties);
+            $("#user-two-div").append("<br>");
         } else {
             $("#user-input").show();
             playerTwo = null;
@@ -126,7 +133,7 @@ $(document).ready(function () {
         }
 
         if (playerOne && playerTwo) {
-            $("#user-one-choice").css("border", "1px solid #3acbe8");
+            $("#user-one-div").css("border", "1px solid #3acbe8");
             $("#instructions").text("Player 1's turn");
         }
     }, function (errorObject) {
@@ -135,44 +142,60 @@ $(document).ready(function () {
 
     database.ref("/numTurns/").on("value", function (snapshot) {
         if (snapshot.val() === 1) {
-            turn = 1;
-        }
-        if (playerOne && playerTwo) {
-            $("#user-one-choice").css("border", "1px solid #3acbe8");
-            $("#user-two-choice").css("border", "none");
+            numTurns = 1;
+
+            if (playerOne && playerTwo) {
+                $("#user-one-div").css("border", "1px solid #3acbe8");
+                $("#user-two-div").css("border", "none");
+                $("#user-choices").show();
+                $("#opponent-choices").hide();
+                $("#instructions").text("Waiting on player 1 to choose");
+            }
         } else if (snapshot.val() === 2) {
-            
+            numTurns = 2;
+
+            if (playerOne && playerTwo) {
+                $("#user-two-div").css("border", "1px solid #3acbe8");
+                $("#user-one-div").css("border", "none");
+                $("#opponent-choices").show();
+                $("#user-choices").hide();
+                $("#instructions").text("Waiting on player 2 to choose");
+            }
         }
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
 
-    //get users choice on click
-    $("#user-choices").on("click", ".choices", function(event) {
-        event.preventDefault();
+    //get users choice on click & track it in the db
+    $("#user-choices").on("click", ".choices", function (event) {
 
-        if ((numTurns === 1) ) {
+        if ((numTurns === 1)) {
             var choice = $(this).attr("data-name");
-            $("#user-one-choice").append("You chose: " + choice);
             playerOneChoice = choice;
+            $("#user-one-choice").text("You chose: " + playerOneChoice);
             console.log("Player One Choice: " + playerOneChoice)
-            database.ref().child("/players/playerOne/choice").set(playerOne.choice);
+            database.ref().child("/players/playerOne/choice").set(playerOneChoice);
             numTurns = 2;
+            console.log(numTurns);
+            //setting numTurns to 2 in the db
             database.ref().child("/numTurns").set(2);
         }
 
     });
 
 
-    //get opponents choice on click
-    $("#opponent-choices").on("click", ".choices", function(event) {
-        event.preventDefault();
+    //get opponents choice on click & track it in the db
+    $("#opponent-choices").on("click", ".choices", function (event) {
 
-        if ((numTurns === 2) ) {
+        if ((numTurns === 2)) {
             var choice = $(this).attr("data-name");
-            $("#user-two-choice").append("You chose: " + choice);
             playerTwoChoice = choice;
+            $("#user-two-choice").text("You chose: " + playerTwoChoice);
             console.log("Player Two Choice: " + playerTwoChoice)
-            database.ref().child("/players/playerTwo/choice").set(playerTwo.choice);
+            database.ref().child("/players/playerTwo/choice").set(playerTwoChoice);
             numTurns = 1;
+            console.log(numTurns);
+            //setting numTurns to 1 in the db
             database.ref().child("/numTurns").set(1);
             whoWins();
         }
@@ -193,16 +216,10 @@ $(document).ready(function () {
             btn.addClass("choices");
             btn.attr("data-name", choices[i]);
             btn.text(choices[i]);
-            $("#user-choices").append(btn);
+            $(btn).clone().appendTo("#opponent-choices");
+            $(btn).clone().appendTo("#user-choices");
         }
     }
-
-    // function getUserChoice() {
-    //     var choice = $(this).attr("data-name");
-    //     $("#user-one-choice").append("You chose: " + choice);
-    // }
-
-    // $(document).on("click", ".choices", getUserChoice);
 
     //clearing the database when a user signs out
     $("#sign-out").on("click", function (event) {
@@ -210,7 +227,6 @@ $(document).ready(function () {
     });
 
     renderButtons();
-
 
 });
 
@@ -259,17 +275,6 @@ $(document).ready(function () {
 
         // //getting user choice on choice button click
         // $(document).on("click", ".choices", getUserChoice);
-
-        // database.ref().on("value", function (snapshot) {
-        //     // console.log(snapshot.val().name);
-        //     // console.log(snapshot.val().choice);
-        //     $("#user-name").text("Hello, " + snapshot.val().thisPlayer);
-
-        // }, function (errorObject) {
-        //     console.log("Errors handled: " + errorObject.code);
-        // });
-
-        // renderButtons();
 
         // // creating a function to clear the database to be used when users sign out or time out
         // function clearFirebase () {
